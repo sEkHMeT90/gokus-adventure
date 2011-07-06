3054d7ab3c2d9d6aa248e51bdb70a674d4f2926e
andresmarotta@gmail.com <andresmarotta@gmail.com@8d909e90-988a-11a0-3adf-c4c4915fcc36>
2011-07-06 06:44:51 +0000

    Resubida de los fuentes "Ayuda", "Mapa", "Nivel", "CargarGuardar", "Plano" y "Enemigo".

---
+++ trunk/ gokus-adventure/fuentes/Ayuda.cs
@@ -0,0 +1,21 @@
+﻿/** 
+ *   Ayuda: Contiene la información de los carteles de ayuda del nivel
+ **/
+
+/* --------------------------------------------------         
+   Goku's Adventure
+   Versiones hasta la fecha:
+   
+   Num.   Fecha       Por / Cambios
+   ---------------------------------------------------
+   0.01  05-Jul-2011  Andrés Marotta
+                      Version inicial: Creada la clase "Ayuda".
+                      
+ ---------------------------------------------------- */
+
+using System;
+
+class Ayuda
+{
+  // TODO
+}
---
+++ trunk/ gokus-adventure/proyectoVS2008/Nivel.cs
@@ -0,0 +1,33 @@
+﻿/** 
+ *   Nivel: Contiene la información del nivel actual
+ **/
+
+/* --------------------------------------------------         
+   Goku's Adventure
+   Versiones hasta la fecha:
+   
+   Num.   Fecha       Por / Cambios
+   ---------------------------------------------------
+   0.01  05-Jul-2011  Andrés Marotta
+                      Version inicial: Creada la clase "Nivel".
+                      Agregados los atributos "ayuda" y "enemigo"
+                        para enlazar con las clases "Ayuda" y "Enemigo"
+                        respectivamente.
+                      
+ ---------------------------------------------------- */
+
+using System;
+
+class Nivel
+{
+  private Ayuda ayuda;
+  private Enemigo enemigo;
+
+  public Nivel()
+  {
+    ayuda = new Ayuda();
+    enemigo = new Enemigo();
+  }
+
+  // TODO
+}
