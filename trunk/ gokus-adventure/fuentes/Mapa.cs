c0410db1ea6031252839822e0c4d225ab077c159
ncabanes <ncabanes@8d909e90-988a-11a0-3adf-c4c4915fcc36>
2011-07-06 21:41:26 +0000

    Version 0.01 restructurada (segundo acercamiento)

---
+++ trunk/ gokus-adventure/fuentes/Mapa.cs
@@ -0,0 +1,30 @@
+﻿/** 
+ *   Mapa: Contiene la información del nivel actual
+ **/
+
+/* --------------------------------------------------         
+   Goku's Adventure
+   Versiones hasta la fecha:
+   
+   Num.   Fecha       Por / Cambios
+   ---------------------------------------------------
+   0.01  05-Jul-2011  Andrés Marotta
+                      Version inicial: Creada la clase "Mapa".
+                      Agregado el atributo "nivel" para enlazar
+                        con la clase "Nivel".
+                      
+ ---------------------------------------------------- */
+
+using System;
+
+class Mapa
+{
+    private Nivel nivelActual;
+
+    public Mapa()
+    {
+        nivelActual = new Nivel();
+    }
+
+    // TODO
+}
