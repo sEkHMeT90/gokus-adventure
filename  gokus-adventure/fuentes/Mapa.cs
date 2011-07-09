21643b7f8e6a5655447c37cbb2e0a835474cb99a
pedro.zalacain@gmail.com <pedro.zalacain@gmail.com@8d909e90-988a-11a0-3adf-c4c4915fcc36>
2011-07-09 14:35:16 +0000

    Modificados fuentes "Mapa.cs" y "Nivel.cs", agregado "Nivel01.cs". Añadidas imagenes, para mostrar un mapa de ejemplo.
    Comentario:
    Se han modificado los fuentes "Personaje.cs" y "Partida.cs" pero no se han subido, a la espera de otras modificaciones.

---  gokus-adventure/fuentes/Mapa.cs
+++  gokus-adventure/fuentes/Mapa.cs
@@ -1,30 +1,144 @@
 ﻿/** 
- *   Mapa: Contiene la información del nivel actual
+ *   Mapa: 
  **/
 
 /* --------------------------------------------------         
    Goku's Adventure
    Versiones hasta la fecha:
-   
+
    Num.   Fecha       Por / Cambios
    ---------------------------------------------------
-   0.01  05-Jul-2011  Andrés Marotta
-                      Version inicial: Creada la clase "Mapa".
-                      Agregado el atributo "nivel" para enlazar
-                        con la clase "Nivel".
-                      
+   0.01  09-Jul-2011  Pedro Zalacain
+   					   Creada la clase
+   					   Creado DibujarOculta aplicando Scroll
+   					   Enlazada clase Mapa con Nivel
+                       Funcion ReiniciarNivel() -> (@see: Nivel.cs)               
+                       Funcion ColisionElementos() -> (@see: Nivel.cs)
  ---------------------------------------------------- */
 
-using System;
-
-class Mapa
+public class Mapa
 {
-    private Nivel nivelActual;
+    Nivel nivelActual;
+    Nivel[] listaNiveles;
+    Partida miPartida;
+    Personaje miPersonaje;
 
-    public Mapa()
+    const int MAX_NIVELES = 1;
+    int numeroNivelActual = 0;
+
+    // Constructor
+    public Mapa(Partida p, Personaje pers)
     {
-        nivelActual = new Nivel();
+        miPartida = p;
+        miPersonaje = pers;
+
+        listaNiveles = new Nivel[MAX_NIVELES];
+        listaNiveles[0] = new Nivel01(this);
+        // listaNiveles[1] = new Nivel02(this);
+
+        nivelActual = listaNiveles[numeroNivelActual];
     }
 
-    // TODO
-}
+    public void Reiniciar()
+    {
+        numeroNivelActual = 0;
+        nivelActual = listaNiveles[numeroNivelActual];
+        nivelActual.Reiniciar();
+    }
+
+    public void ReiniciarNivel()
+    {
+        nivelActual.Reiniciar();
+    }
+
+    public void DibujarOculta(int valor)
+    {
+        nivelActual.DibujarOculta(valor);
+    }
+
+    public bool EsPosibleMover(int x, int y, int xmax, int ymax, int scroll)
+    {
+        return nivelActual.EsPosibleMover(x, y, xmax, ymax, scroll);
+    }
+
+    public bool EsPosibleCaer(int x, int y, int xmax, int ymax, int scroll)
+    {
+        return nivelActual.EsPosibleCaer(x, y, xmax, ymax, scroll);
+    }
+
+    public void Avanzar()
+    {
+        numeroNivelActual++;
+        if (numeroNivelActual >= MAX_NIVELES)
+            numeroNivelActual = 0;
+
+        nivelActual = listaNiveles[numeroNivelActual];
+
+        miPersonaje.MoverA(400, 395);
+        // miPersonaje.SetMaxMinX(2580, 250);
+        // miPartida.SetScroll(-104);
+
+        nivelActual.Reiniciar();
+
+        /*
+
+        // Borro los disparos (aparecerian en el siguiente nivel si el personaje justo dispara)
+        for (int x = 0; x < miPersonaje.GetCantidadDisparos() - 1; x++)
+            miPersonaje.GetDisparo(x).Reiniciar();
+          
+        */ 
+
+        // Pequeña pausa antes de lanza el nuevo nivel
+        System.Threading.Thread.Sleep(1000);
+
+        /*
+        // Usado en GnG (Pedro Zalacain) para el mostrar el mapa intermedio
+        miPartida.SetAvanzada(true);
+        */
+    }
+
+    public int GetMaxX()
+    {
+        return nivelActual.GetMaxX();
+    }
+
+
+    public int GetMinX()
+    {
+        return nivelActual.GetMinX();
+    }
+
+
+    public int GetMaxY()
+    {
+        return nivelActual.GetMaxY();
+    }
+
+
+    public int GetMinY()
+    {
+        return nivelActual.GetMinY();
+    }
+    /*
+    public int GetNumEnemigos()
+    {
+        return nivelActual.GetNumEnemigos();
+    }
+
+    public Enemigo GetEnemigo(int i)
+    {
+        return nivelActual.GetEnemigo(i);
+    }
+    */
+    public void SetNivel(int valor) // Empleado al cargar Partida
+    {
+        numeroNivelActual = valor;
+        nivelActual = listaNiveles[numeroNivelActual];
+    }
+
+    public int GetNivel()
+    {
+        return numeroNivelActual;
+    }
+
+} /* fin de la clase Mapa */
