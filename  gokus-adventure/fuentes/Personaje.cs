1e6424cb7d393dcbd75a8740a13c93920ca4ef7e
andresmarotta@gmail.com <andresmarotta@gmail.com@8d909e90-988a-11a0-3adf-c4c4915fcc36>
2011-08-15 17:05:00 +0000

    Modificadas las imágenes de Goku, ahora son gif's a 50x50 píxeles. Modificada la clase "Personaje". Actualizado el Diagrama de Clases.

---  gokus-adventure/fuentes/Personaje.cs
+++  gokus-adventure/fuentes/Personaje.cs
@@ -60,40 +60,43 @@ public class Personaje : ElemGrafico
     toques = 57;
 
     CargarSecuencia( DERECHA,
-                      new string[] { "imagenes/Goku/caminandoD1.png", "imagenes/Goku/caminandoD1.png",
-                                     "imagenes/Goku/caminandoD2.png", "imagenes/Goku/caminandoD2.png",
-                                     "imagenes/Goku/caminandoD3.png", "imagenes/Goku/caminandoD3.png",
-                                     "imagenes/Goku/caminandoD4.png", "imagenes/Goku/caminandoD4.png",
-                                     "imagenes/Goku/caminandoD5.png", "imagenes/Goku/caminandoD5.png",
-                                     "imagenes/Goku/caminandoD6.png", "imagenes/Goku/caminandoD6.png",
-                                     "imagenes/Goku/caminandoD7.png", "imagenes/Goku/caminandoD7.png",
-                                     "imagenes/Goku/caminandoD8.png", "imagenes/Goku/caminandoD8.png"} );
+                      new string[] { "imagenes/Goku/caminandoD1.gif", "imagenes/Goku/caminandoD1.gif",
+                                     "imagenes/Goku/caminandoD2.gif", "imagenes/Goku/caminandoD2.gif",
+                                     "imagenes/Goku/caminandoD3.gif", "imagenes/Goku/caminandoD3.gif",
+                                     "imagenes/Goku/caminandoD4.gif", "imagenes/Goku/caminandoD4.gif",
+                                     "imagenes/Goku/caminandoD5.gif", "imagenes/Goku/caminandoD5.gif",
+                                     "imagenes/Goku/caminandoD6.gif", "imagenes/Goku/caminandoD6.gif",
+                                     "imagenes/Goku/caminandoD7.gif", "imagenes/Goku/caminandoD7.gif",
+                                     "imagenes/Goku/caminandoD8.gif", "imagenes/Goku/caminandoD8.gif"} );
 
     CargarSecuencia( IZQUIERDA,
-                      new string[] { "imagenes/Goku/caminandoI1.png", "imagenes/Goku/caminandoI1.png",
-                                     "imagenes/Goku/caminandoI2.png", "imagenes/Goku/caminandoI2.png",
-                                     "imagenes/Goku/caminandoI3.png", "imagenes/Goku/caminandoI3.png",
-                                     "imagenes/Goku/caminandoI4.png", "imagenes/Goku/caminandoI4.png",
-                                     "imagenes/Goku/caminandoI5.png", "imagenes/Goku/caminandoI5.png",
-                                     "imagenes/Goku/caminandoI6.png", "imagenes/Goku/caminandoI6.png",
-                                     "imagenes/Goku/caminandoI7.png", "imagenes/Goku/caminandoI7.png",
-                                     "imagenes/Goku/caminandoI8.png", "imagenes/Goku/caminandoI8.png"} );
+                      new string[] { "imagenes/Goku/caminandoI1.gif", "imagenes/Goku/caminandoI1.gif",
+                                     "imagenes/Goku/caminandoI2.gif", "imagenes/Goku/caminandoI2.gif",
+                                     "imagenes/Goku/caminandoI3.gif", "imagenes/Goku/caminandoI3.gif",
+                                     "imagenes/Goku/caminandoI4.gif", "imagenes/Goku/caminandoI4.gif",
+                                     "imagenes/Goku/caminandoI5.gif", "imagenes/Goku/caminandoI5.gif",
+                                     "imagenes/Goku/caminandoI6.gif", "imagenes/Goku/caminandoI6.gif",
+                                     "imagenes/Goku/caminandoI7.gif", "imagenes/Goku/caminandoI7.gif",
+                                     "imagenes/Goku/caminandoI8.gif", "imagenes/Goku/caminandoI8.gif"} );
 
     CargarSecuencia( ESPERANDOD,
-                      new string[] { "imagenes/Goku/paradoD1.png", "imagenes/Goku/paradoD1.png",
-                                     "imagenes/Goku/paradoD2.png", "imagenes/Goku/paradoD2.png",
-                                     "imagenes/Goku/paradoD3.png", "imagenes/Goku/paradoD3.png",
-                                     "imagenes/Goku/paradoD4.png", "imagenes/Goku/paradoD4.png",
-                                     "imagenes/Goku/paradoD5.png", "imagenes/Goku/paradoD5.png",
-                                     "imagenes/Goku/paradoD6.png", "imagenes/Goku/paradoD6.png"} );
+                      new string[] { "imagenes/Goku/paradoD1.gif", "imagenes/Goku/paradoD1.gif",
+                                     "imagenes/Goku/paradoD1.gif", "imagenes/Goku/paradoD2.gif",
+                                     "imagenes/Goku/paradoD2.gif", "imagenes/Goku/paradoD2.gif",
+                                     "imagenes/Goku/paradoD3.gif", "imagenes/Goku/paradoD3.gif",
+                                     "imagenes/Goku/paradoD3.gif", "imagenes/Goku/paradoD4.gif",
+                                     "imagenes/Goku/paradoD4.gif", "imagenes/Goku/paradoD4.gif",
+                                     "imagenes/Goku/paradoD5.gif", "imagenes/Goku/paradoD5.gif",
+                                     "imagenes/Goku/paradoD5.gif", "imagenes/Goku/paradoD6.gif",
+                                     "imagenes/Goku/paradoD6.gif", "imagenes/Goku/paradoD6.gif"} );
 
     CargarSecuencia( ESPERANDOI,
-                      new string[] { "imagenes/Goku/paradoI1.png", "imagenes/Goku/paradoI1.png",
-                                     "imagenes/Goku/paradoI2.png", "imagenes/Goku/paradoI2.png",
-                                     "imagenes/Goku/paradoI3.png", "imagenes/Goku/paradoI3.png",
-                                     "imagenes/Goku/paradoI4.png", "imagenes/Goku/paradoI4.png",
-                                     "imagenes/Goku/paradoI5.png", "imagenes/Goku/paradoI5.png",
-                                     "imagenes/Goku/paradoI6.png", "imagenes/Goku/paradoI6.png"} );
+                      new string[] { "imagenes/Goku/paradoI1.gif", "imagenes/Goku/paradoI1.gif",
+                                     "imagenes/Goku/paradoI2.gif", "imagenes/Goku/paradoI2.gif",
+                                     "imagenes/Goku/paradoI3.gif", "imagenes/Goku/paradoI3.gif",
+                                     "imagenes/Goku/paradoI4.gif", "imagenes/Goku/paradoI4.gif",
+                                     "imagenes/Goku/paradoI5.gif", "imagenes/Goku/paradoI5.gif",
+                                     "imagenes/Goku/paradoI6.gif", "imagenes/Goku/paradoI6.gif"} );
 
     CargarSecuencia( GIRANDOPALO,
                       new string[] { /*"imagenes/Goku/palo1.png", "imagenes/Goku/palo1.png",
